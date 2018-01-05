60af349abc119dd6283b2a7f7e910f4d6f774bfa
hshinz <whyisme110222@gmail.com>
2018-01-05 19:34:59 +0700

    Add mail server production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,6 +104,18 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
     end
+  # Gmail configuration
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['EMAIL_USER'],
+    password:             ENV['EMAIL_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+}
 
 
   # Do not dump schema after migrations.
