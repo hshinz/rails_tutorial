30449cc1e77fb5f1f965e6d0508b74a6eb50fe4f
hshinz <whyisme110222@gmail.com>
2018-01-05 19:05:38 +0700

    Finish activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
   end
 
   def new
@@ -16,10 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
-        log_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -45,15 +45,16 @@ class UsersController < ApplicationController
   # Before filters
 
   # Confirms a logged-in user.
-  def logged_in_user
+    def logged_in_user
       unless logged_in?
         store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
-  end
+    end
     # Confirms the correct user.
-  def correct_user
+    def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
   end
 end
