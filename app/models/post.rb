4cd56925bf81a495898ce09ce326b74c5fdc597d
hshinz <whyisme110222@gmail.com>
2018-01-15 21:08:51 +0700

    Comment section

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+ # belongs_to :user
+  belongs_to :creator,
+   class_name: User.name, foreign_key: :user_id
   self.per_page = 10
+  mount_uploader :image_url, PictureUploader
 end
